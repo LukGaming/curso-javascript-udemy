d0b4812459d6587a13cc4c0d289311341884d2de
LukGaming <thelukgaming333@gmail.com>
2021-12-30 10:14:17 -0300

    Arguments and calcula media

---
+++ calcula_media/calcula_media.js
@@ -0,0 +1,19 @@
+function calculaMedia() {
+    //Caso não for passados parametros para a função, retornar zero
+    console.log(arguments)
+    if (arguments.length == 0) {
+        return 0;
+    }
+    var soma = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] != 'number') {
+            return "Está função recebe somente números";
+        }
+        else{
+            soma += arguments[i];
+        }
+    }
+    return soma / arguments.length;
+    
+}
+console.log(calculaMedia(2, 3, 1))
